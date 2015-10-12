3ae7a401507add460715e6dc8d752e8a569b41e2
Janis Lesinskis <footbagfreestyle@gmail.com>
2015-10-11 21:20:20 -0400

    Started creating setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(name='mmap_backed_array',
+      version='0.1.0',
+      description='mmap backed Array for Python',
+      long_description=open('README.rst').read(),
+      url='https://github.com/JaggedVerge/mmap_backed_array',
+      packages=['mmap_backed_array'],
+      classifiers = [
+          'Development Status :: 3 - Alpha',
+          'Intended Audience :: Developers',
+          'Programming Language :: Python',
+      ],
+      keywords='mmap array',
+)
