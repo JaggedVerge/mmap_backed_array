fea06f00240bed10ae98e7c55b4546e77ab4e10f
Janis Lesinskis <footbagfreestyle@gmail.com>
2015-11-30 20:47:06 -0500

    bump version number

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='mmap_backed_array',
-      version='0.1.0',
+      version='0.2.0',
       description='mmap backed Array for Python',
       long_description=open('README.rst').read(),
       url='https://github.com/JaggedVerge/mmap_backed_array',
