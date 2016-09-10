5bbadbcfb6d4373a15ea9f90f680fd8d7b2090e9
Janis <janis@jaggedverge.com>
2016-09-10 22:26:19 +1000

    Coverage tests for bad input

--- mmap_backed_array/tests/test_slice_decoding.py
+++ mmap_backed_array/tests/test_slice_decoding.py
@@ -9,3 +9,14 @@ def test_decode_slice_bad_parameter():
     """Test that bad parameters to decode_slice raise"""
     with pytest.raises(TypeError):
         _decode_slice("this is not a slice object", 10)
+
+    s = slice(1,2,3)
+    #sice must be an integer
+    with pytest.raises(TypeError):
+        _decode_slice(s, "This is not a real size")
+    with pytest.raises(TypeError):
+        _decode_slice(s, 0.12345)
+
+    #size can't be negative
+    with pytest.raises(ValueError):
+        _decode_slice(s, -1)
